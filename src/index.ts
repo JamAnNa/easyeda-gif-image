fc78839ccfdc830aa3a5e04937454e0cf4941b5a
IsAnAn <3599766025@qq.com>
2025-10-16 09:59:35 +0800

    拉取基础SDK，第一次构建

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function activate(status ?: 'onStartupFinished', arg ?: string) : void { }
+
+export function about() : void {
+	// 此处示例加载扩展 /iframe/ 目录下的 index.html 文件，窗口的高度和宽度均为 500px
+	eda.sys_IFrame.openIFrame('/iframe/index.html', 500, 500);
+}
